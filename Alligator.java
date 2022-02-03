4c8c509c8d2b1830bd4990acd732e016dc538b05
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-03 23:11:56 +0530

    Add files via upload

---
+++ Alligator.java
@@ -0,0 +1,11 @@
+package Dhanarjanudip06;
+class Alligator  { 
+public static void main(String[] args)
+ { 
+int []x[] = {{1,2}, {3,4,5}, {6,7,8,9}}; 
+int [][]y = x; 
+System.out.println(y[2][1]);
+ } }
+
+//O/p
+//7
