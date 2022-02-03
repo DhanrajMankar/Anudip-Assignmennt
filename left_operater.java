d96d964176b87fea8f6fd0d8430cfb3af49d2260
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-03 23:18:51 +0530

    Add files via upload

---
+++ left_operater.java
@@ -0,0 +1,16 @@
+package Dhanarjanudip06;
+class leftshift_operator
+{ 
+public static void main(String args[]) 
+{ 
+byte x = 64; 
+int i; 
+byte y; 
+i = x << 2;
+y = (byte) (x << 2) ;
+System.out.print(i + " " + y); 
+} }
+
+
+//O/p
+//1
