c173210e8a2661991e85f45d4384f47ad3ffc5c5
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-03 23:14:48 +0530

    Add files via upload

---
+++ output1.java
@@ -0,0 +1,14 @@
+package Dhanarjanudip06;
+class output1 { 
+public static void main(String args[]) 
+{ int y,z;
+	int x=y=z=20;
+	System.out.println(x);
+	System.out.println(y);
+	System.out.println(z);
+ } 
+}
+//O/p
+//20
+//20
+//20
