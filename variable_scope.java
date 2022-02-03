351e98e7794d7fb3f51c84e703ed4f3f37d304d0
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-03 23:16:56 +0530

    Add files via upload

---
+++ variable_scope.java
@@ -0,0 +1,17 @@
+package Dhanarjanudip06;
+class variable_scope
+{
+public static void main(String args[])
+{ 
+int x;
+x = 5; 
+{
+int y = 6; 
+System.out.print(x + " " + y); 
+} 
+System.out.println(x + " " + x);
+}
+}
+//O/p
+//6
+//5
