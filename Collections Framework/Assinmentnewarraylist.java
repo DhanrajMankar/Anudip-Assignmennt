1f0ac9da72538bc392faabf199433037fe19fbe2
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-09 12:33:45 +0530

    Add files via upload

---
+++ Collections Framework/Assinmentnewarraylist.java
@@ -0,0 +1,16 @@
+package Dhanarjanudip06;
+import java.util.*;
+public class Assinmentnewarraylist {
+
+	public static void main(String[] args) 
+	{
+		LinkedList<String> list=new LinkedList<String>(); 
+		list.add("black");//Adding object in arraylist1 
+		list.add("white");  
+		list.add("blue");  
+		list.add("pink"); 
+		list.add("red"); 
+		System.out.println(list);
+	}
+
+}
