4e63961582dcae2751b4e9b0495771cffe91c352
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-09 12:34:58 +0530

    Add files via upload

---
+++ Collections Framework/assinmentaarraylistupdate.java
@@ -0,0 +1,18 @@
+package Dhanarjanudip06;
+import java.util.*;
+public class assinmentaarraylistupdate {
+
+	public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+		al.add("red");
+		al.add("pink");
+		al.add("blue");
+		al.add("orange");
+		System.out.println(al);
+		//Updating the second element in array list 
+	    al.set(1, "white");
+	    //pnk chnnages  to white
+		System.out.println(al);
+	}
+
+}
