ec39c946f8b9b70e17a002ec9c2dc72f57ee7de9
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-09 12:36:05 +0530

    Add files via upload

---
+++ Collections Framework/Assinmentreverselinkist.java
@@ -0,0 +1,18 @@
+package Dhanarjanudip06;
+
+import java.util.*;
+public class Assinmentreverselinkist {
+
+	public static void main(String[] args)
+	{
+		LinkedList<String> list=new LinkedList<String>(); 
+		list.add("black");//Adding object in arraylist1 
+		list.add("white");  
+		list.add("blue");  
+		list.add("pink"); 
+		list.add("red"); 
+		Collections.reverse(list);
+	System.out.println(list);
+	}
+
+}
