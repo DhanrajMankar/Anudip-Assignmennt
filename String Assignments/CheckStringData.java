d2530371a3b3d95c5ed4bb30dc24d27136ea5bfe
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-17 09:46:33 +0530

    Add files via upload

---
+++ String Assignments/CheckStringData.java
@@ -0,0 +1,17 @@
+package Dhanarjanudip06;
+
+import java.util.Scanner;
+
+public class CheckStringData {
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter first String");
+		String a1=s.next();
+		System.out.println("Enter second String");
+		String a2=s.next();
+		System.out.println(a1.equals(a2));
+		
+		
+	}
+
+}
