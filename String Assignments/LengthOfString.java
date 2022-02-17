df676bef006205251f015d2132a251ec99125086
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-17 09:38:51 +0530

    Add files via upload

---
+++ String Assignments/LengthOfString.java
@@ -0,0 +1,14 @@
+package Dhanarjanudip06;
+
+import java.util.Scanner;
+
+public class LengthOfString {
+
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter The String");
+		String a1=s.next();
+		System.out.println("The Length of is String"+a1.length());
+	}
+
+}
