c02ca9c43ef402b07711c23f3b22f5b7e260a5cf
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-17 09:40:04 +0530

    Add files via upload

---
+++ String Assignments/LowerCase.java
@@ -0,0 +1,14 @@
+package Dhanarjanudip06;
+
+import java.util.Scanner;
+
+public class LowerCase {
+
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter The String");
+		String a1=s.next();
+		System.out.println(a1.toLowerCase());
+	}
+
+}
